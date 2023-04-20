51d5afcf36a52ffbda9a5e1bf26a3461067e2004
SambuK18 <55884091+Sambuk18@users.noreply.github.com>
2023-04-20 15:09:42 -0300

    Codigo Inicial de Prueba

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>IES Juan Mantovani</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+  <h1>Bienvenidos Hola Mundo</h1>
+  <h2>ChoicesPracticas Profesionalizantes</2>
+</body>
+</html>
